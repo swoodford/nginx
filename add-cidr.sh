8027477e7e536c453cb7709899155a7ad2cf4f59
Shawn Woodford <sw@shawnwoodford.com>
2015-02-25 11:09:13 -0500

    uncomment working code

--- add-cidr.sh
+++ add-cidr.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 # This script will add CIDR notation to any IPs missing /32 /24 or /14 CIDR notation
 
-# LIST=$(cat blacklist | nl)
-# TOTAL=$(cat blacklist | wc -l)
-
-
-# START=1
-# for (( COUNT=$START; COUNT<=$TOTAL; COUNT++ ))
-# do
-# 	IP=$(echo "$LIST" | grep -w [^0-9][[:space:]]$COUNT | cut -f 2))
-# 	if egrep -qv '/32$' $IP; then
-# 		if egrep -qv '/24$' $IP; then
-# 			if egrep -qv '/14$' $IP; then
-
-
-
-
 # cat blacklist | egrep '/32$'
-
-
-
 
 # while read blacklist
 # do
@@ -32,13 +14,13 @@
 # 	fi
 # done < blacklist
 
-# while read blacklist
-# do
-# 	if ! egrep -qv '/32$' $blacklist; then
-# 		if ! egrep -qv '/24$' $blacklist; then
-# 			if ! egrep -qv '/14$' $blacklist; then
-# 				echo "$blacklist/32" >> blacklist3
-# 			fi
-# 		fi
-# 	fi
-# done < blacklist
+while read blacklist
+do
+	if ! egrep -qv '/32$' $blacklist; then
+		if ! egrep -qv '/24$' $blacklist; then
+			if ! egrep -qv '/14$' $blacklist; then
+				echo "$blacklist/32" >> blacklist3
+			fi
+		fi
+	fi
+done < blacklist
